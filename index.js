82da562a3450c153c7d76e03933f5f766ce6f45a
And4Web <anands2807@gmail.com>
2023-01-25 12:27:12 +0530

    globals notes created

--- index.js
+++ index.js
@@ -27,5 +27,14 @@ const map = new Map();
 map.set('West Bengal', 'Kolkata');
 map.set('Karnataka', 'Bengaluru');
 params = new URLSearchParams(map);
-params.set("Jammu", "Kashmir")
-console.log("https://geeksforgeeks.org/p/a/t/h?query=string&" + params.toString());
+params.set("Jammu", "Kashmir");
+params.sort();
+// console.log("https://geeksforgeeks.org/p/a/t/h?query=string&" + params.toString());
+/*for(let pair of params.entries()){
+  console.log(pair[0] + ' >===> ' + pair[1] );
+}
+*/
+// let someUrl =new URL(myUrl) 
+// console.log(someUrl.searchParams.forEach((value, name) => {
+//   console.log(name + " >====> " + value)
+// }))
