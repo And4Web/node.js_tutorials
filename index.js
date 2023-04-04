067053c2840b798b106ec6b3cf09b9afd33c14d6
And4Web <anands2807@gmail.com>
2023-04-04 16:07:12 +0530

    eventEmitter

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 var os = require('os');
+const stream = require('stream');
+const {EventEmitter} = require('events');
 
+const myEvent = new EventEmitter();
+// console.log(myEvent)
+
+myEvent.on('event', ()=>{
+  console.log("event listened as event has emitted.")
+})
+myEvent.on("event", ()=>{
+  console.log('this is the second user')
+})
+
+myEvent.emit("event", ()=>{
+  console.log('event emitted.')
+})
---
+++ streams.js
@@ -0,0 +1,1 @@
+//from: https://www.geeksforgeeks.org/node-js-streams/?ref=lbp
